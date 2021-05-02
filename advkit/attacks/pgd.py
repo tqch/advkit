fix gradient accumulation problem

advkit/attacks/pgd.py
@@ -64,14 +64,14 @@ class PGD:
         out_adv = model(x_adv)
         if isinstance(out_adv, (list, tuple)):
             out_adv = out_adv[-1]
-        pred_adv = self.out_activation(out_adv)
+        out_adv = self.out_activation(out_adv)
         if self.targeted:
             assert targets is not None, "Target labels not found!"
-            loss = self.loss_fn(pred_adv, targets)
+            loss = self.loss_fn(out_adv, targets)
         else:
-            loss = self.loss_fn(pred_adv, y)
-        loss.backward()
-        pert = self.step_size / self.std * x_adv.grad.sign()
+            loss = self.loss_fn(out_adv, y)
+        grad = torch.autograd.grad(loss, x_adv)[0]
+        pert = self.step_size / self.std * grad.sign()
         x_adv = self._clip(x_adv.data + pert)
         pert = self._clip(x_adv, center=x, eps=self.eps) - x
         return x + pert

advkit/defenses/trades.py
@@ -146,10 +146,9 @@ def trades(
 
 
 if __name__ == "__main__":
-    import math
     from advkit.convnets.vgg import VGG
     from advkit.utils.data import WEIGHTS_FOLDER, DATA_PATH, DATASET_CONFIGS
-    from torch.optim import SGD, lr_scheduler
+    from torch.optim import SGD
 
     model = VGG.from_default_config("vgg16")
     model.to(DEVICE)
@@ -195,14 +194,9 @@ if __name__ == "__main__":
             weight_decay=1e-4,
             nesterov=True
         )
-        scheduler = lr_scheduler.LambdaLR(
-            optimizer,
-            lr_lambda=lambda epoch: lr - 0.45 * lr * (1 - math.cos(math.pi * epoch / epochs))
-        )
         trades(
             model,
             optimizer=optimizer,
-            scheduler=scheduler,
             mean=(0, 0, 0),
             std=(1, 1, 1),
             epochs=epochs,